a47661ce3ab945baca55fd871c08dcee285f6adc
Tobias Frank <tobias@Tobiass-MacBook-Pro.local>
2018-04-24 13:17:58 +0200

    feature(router.js):add backend router

---
+++ BackEnd/Router/router.js
@@ -0,0 +1,53 @@
+"use strict";
+
+
+const bodyParser = require('body-parser');
+//import bodyParser from 'body-parser';
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/stundenplaner');
+const express = require('express');
+const app = express.Router();
+
+
+//import schemas from '../Schemas/schemas';
+const schemas = require('../Schemas/schemas');
+
+
+
+
+app.use(bodyParser.json());
+app.use(function (req, res, next) {
+    console.log(req.body);
+    next();
+});
+
+let adress = mongoose.model('Adress', schemas.adress);
+
+app.route('/')
+    .get((req, res, next) => {
+        adress.find({}, function (err, adress) {
+            if (err) throw err;
+            res.status(200).json(adress);
+        });
+    });
+
+
+
+app.all('*', (req, res, next) => {
+    res.status(404).set('Content-Type', 'text/html');
+
+    // respond with html page
+    if (req.accepts('html')) {
+        res.send('404 Not found');
+    }
+    // respond with json
+    else if (req.accepts('json')) {
+        res.send('404 (json) Not found');
+    }
+    // default to plain-text
+    else {
+        res.send('404 (Text) Not found');
+    }
+});
+
+module.exports = app;
