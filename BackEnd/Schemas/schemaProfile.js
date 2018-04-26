ff16b9469db387e3d9279abac329561425952a3e
Tobias Frank <tobias.framl@outlook.de>
2018-04-26 09:27:18 +0200

    feature(schemaProfile.js):edit Profile Attributes

--- BackEnd/Schemas/schemaProfile.js
+++ BackEnd/Schemas/schemaProfile.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 const Address = require('schemaAdress');
+const Subject = require('schemaAdress');
 
 let profile = new Schema({
     name: {type: String, required: true},
-    surName: {type: String, required: true},
+    surname: {type: String, required: true},
     role: [Role],
     dateOfBirth: {type: Date, required: true},
     address: Address,
@@ -12,10 +13,10 @@ let profile = new Schema({
     nationality: {type: String, required: true},
     email: {type: String, required: true},
     contact: {type: String, required: false},
-    contactPhoneNumber: {type: String, required: true},
+    PhoneNumber: {type: String, required: true},
     hoursPerWeek: {type: Number, required: false},
-    specialisation: {type: String, required: false},
-    subjects: {type: Number, required: false}
+    subjectSpecialisationTeacher: [Subject],
+    subjectSpecialisationStudent: [Subject]
 });
 
 module.exports = profile;
