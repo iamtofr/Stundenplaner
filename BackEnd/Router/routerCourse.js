38d34e932d0df519fa8878a41c668b29d46cf79f
Tobias Frank <tobias.framl@outlook.de>
2018-05-08 14:07:36 +0200

    feature(allSchemas):renaming and modifying schemas ala Thomas

--- BackEnd/Router/routerCourse.js
+++ BackEnd/Router/routerCourse.js
@@ -14,7 +14,7 @@ app.use(function (req, res, next) {
     next();
 });
 
-let schoolClass = mongoose.model('schoolClass', schema.schoolClass);
+let schoolClass = mongoose.model('course', schema.course);
 
 
 //TODO Router
--- BackEnd/Schemas/schemaRole.js
+++ BackEnd/Schemas/schemaRole.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
-const admin = require('./schemaAdministratorRole');
-const student = require('./schemaStudentRole');
-const teacher = require('./schemaTeacherRole');
-const management = require('./schemaManagementRole');
-
 let role = new Schema({
-    type: [admin,student,teacher,management]
+    _id: {type: String, required: true, default: 'Administrator'},
+    read: {type: Boolean, required: true},
+    write: {type: Boolean, required: true},
+    print: {type: Boolean, required: true},
+    create: {type: Boolean, required: true},
+    delete: {type: Boolean, required: true}
 });
 
 module.exports = role;
