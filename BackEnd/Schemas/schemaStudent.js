ac3170d4cf614369258558cad0f938714a735e93
Tobias Frank <tobias.framl@outlook.de>
2018-05-15 11:43:54 +0200

    "feature(schemas):add new schema "

--- BackEnd/Schemas/schemaStudent.js
+++ BackEnd/Schemas/schemaStudent.js
@@ -3,8 +3,7 @@ let Schema = mongoose.Schema;
 
 let student = new Schema({
     profile: {type: Schema.Types.ObjectId, ref: 'profile', required: true},
-    grade: {type: Number, required: true},
-    subjectSpecialiations: {type: [Schema.Types.ObjectId], ref: 'subject', required: true}
+    subjectSpecialisations: {type: [Schema.Types.ObjectId], ref: 'subject', required: true}
 });
 
 module.exports = student;
