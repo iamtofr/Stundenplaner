be51cda1096717775101f11f2cf422ab4d030ee8
Tobias Frank <tobias.framl@outlook.de>
2018-05-16 11:40:25 +0200

    "feature(websocket.js):add websockets feature"

--- BackEnd/SocketIO/websocket.js
+++ BackEnd/SocketIO/websocket.js
@@ -1,11 +1,8 @@
-let socket = () => {
-    io.on('connection', (socket) => {
-        console.log('User connected');
-        socket.on('chat message', (msg) => io.emit('chat message', msg));
-        socket.on('disconnect', () =>
-            console.log('user disconnected'));
-    });
-};
-
-
-module.exports = socket;
+<script src="socket.io.js"></script> <script src="https://code.jquery.com/jquery- 1.11.1.js"></script>
+    <script> $(function () {
+    var socket = io();
+    $('form').submit(function() { socket.emit('chat message', $('#m').val());
+        $('#m').val(''); return false; });
+    socket.on('chat message', function(msg) { $('#messages').append($('<li>').text(msg));
+    }); });
+</script>
