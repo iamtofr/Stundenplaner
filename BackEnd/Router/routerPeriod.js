84509601aea821717aaa9566082186f8e7074ba5
Tobias Frank <tobias.framl@outlook.de>
2018-05-28 14:50:18 +0200

    "feature(routerPeriod): add router"

---
+++ BackEnd/Router/routerPeriod.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/stundenplaner');
+const express = require('express');
+const app = express.Router();
+const schema = require('../Schemas/schemas');
+let getPermission;
+
+app.use(bodyParser.json());
+// app.use(function (req, res, next) {
+//     getPermission = perxmission(req);
+//     console.log(req.body);
+//     next();
+// });
+
+let period = mongoose.model('period', schema.period);
+
+app.route('/')
+    .get((req, res, next) => {
+        period.find({}, function (err, period) {
+            if (err) throw err;
+            res.status(200).json(period);
+        });
+    })
+
+    .post((req, res, next) => {
+        let periodList = req.body;
+        for (let p of periodList) {
+            let newPeriod = period(p);
+            newPeriod.save(function (err) {
+                if (err) throw err;
+                console.log('Period created!');
+            });
+        }
+        res.status(201).json(newPeriod)
+    });
+
+app.route('/:id')
+    .get((req, res, next) => {
+        let query = {'_id': req.params.id};
+        period.find(query, function (err, period) {
+            if (err) throw err;
+            res.status(200).json(period);
+        });
+    });
+
+app.all('*', (req, res, next) => {
+    res.status(404).set('Content-Type', 'text/html');
+
+    // respond with html page
+    if (req.accepts('html')) {
+        res.send('404 Not found');
+    }
+    // respond with json
+    else if (req.accepts('json')) {
+        res.send('404 (json) Not found');
+    }
+    // default to plain-text
+    else {
+        res.send('404 (Text) Not found');
+    }
+});
+
+
+module.exports = app;
